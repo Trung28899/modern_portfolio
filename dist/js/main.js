66f6c0962ebf795eaf18cb5ae9da9fa8b6e45395
Trinh Trung <you@example.com>
2020-01-29 21:01:07 -0500

    messed around

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,9 @@
-// Select DOM items - Video 3 in the series
+/*******************************************************
+ * *****************************************************
+ * JAVASCRIPT FOR NAVIGATION BUTTON *
+ ********************************************************
+ ********************************************************/
+
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
@@ -37,17 +42,27 @@ function toggleMenu() {
   }
 }
 
-// ANIMATION CODE: 
+/*******************************************************
+ * *****************************************************
+ * JAVASCRIPT FOR MY WORK PAGE*
+ *******************************************************
+ ********************************************************/
+
+/*******************************************************
+ * *****************************************************
+ * JAVASCRIPT FOR SHOWCASE EXPERIENCE IN ABOUT ME *
+ *******************************************************
+ ********************************************************/
 var slideIndex = 1;
 // Running the function here
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 // n is variable for slideIndex
@@ -65,20 +80,24 @@ function showSlides(n) {
   */
 
   // if n > 3 then slideIndex goes back to 1
-  if (n > slides.length) {slideIndex = 1} 
-  // if slideIndex < 3, default slideIndex to 3   
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  // if slideIndex < 3, default slideIndex to 3
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
   // remove display for all slides
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   // styling for all dots
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
 
   // show the responding display for image and dots
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += "active";
 }
